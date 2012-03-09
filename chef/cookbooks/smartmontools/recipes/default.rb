862ee3fc96820b27fe2ec67a5d5fa82bfcea6f58
Mike Conigliaro <mike@conigliaro.org>
2012-03-08 21:46:24 -0700

    Configure smartmontools

--- chef/cookbooks/smartmontools/recipes/default.rb
+++ chef/cookbooks/smartmontools/recipes/default.rb
@@ -5,3 +5,19 @@
 # Copyright 2012, Michael Paul Thomas Conigliaro
 #
 package "smartmontools"
+
+service "smartmontools" do
+  action :enable
+end
+
+template "/etc/default/smartmontools" do
+  source "smartmontools.default.erb"
+  mode "0644"
+  notifies :restart, "service[smartmontools]"
+end
+
+template "/etc/monit/conf.d/smartmontools.monit" do
+  source "smartmontools.monit.erb"
+  mode "0644"
+  notifies :restart, "service[monit]"
+end
