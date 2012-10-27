a4d75a825e2526847d1849b9cd72049e8cd4c8cf
Mike Conigliaro <mike@conigliaro.org>
2012-10-27 12:58:29 -0600

    Trivial update

--- cookbooks/users/recipes/default.rb
+++ cookbooks/users/recipes/default.rb
@@ -4,44 +4,44 @@
 #
 # Copyright 2012, Michael Paul Thomas Conigliaro
 #
-data_bag("users").map { |obj| data_bag_item("users", obj) }.each do |user|
-  user user["id"] do
-    comment user["comment"]
-    home user["homedir"]
-    shell user["shell"]
+data_bag("users").map { |obj| data_bag_item("users", obj) }.each do |u|
+  user u["id"] do
+    comment u["comment"]
+    home u["homedir"]
+    shell u["shell"]
     supports :manage_home => true # FIXME: Why doesn't this work?
     only_if { node["users"]["manage"] }
   end
 
-  user["groups"].each do |group|
+  u["groups"].each do |group|
     group group do
-      members [user["id"]]
+      members [u["id"]]
       append true
       only_if { node["users"]["manage"] }
     end
   end
 
-  oh_my_zsh user["id"] do
-    manage_zshrc user["oh_my_zsh"]["manage_zshrc"]
-    theme user["oh_my_zsh"]["theme"]
-    plugins user["oh_my_zsh"]["plugins"]
-    only_if { user["shell"] =~ /zsh$/ }
+  oh_my_zsh u["id"] do
+    manage_zshrc u["oh_my_zsh"]["manage_zshrc"]
+    theme u["oh_my_zsh"]["theme"]
+    plugins u["oh_my_zsh"]["plugins"]
+    only_if { u["shell"] =~ /zsh$/ }
   end
 
-  git_config user["id"] do
-    options user["git"]
-    only_if { user["git"] }
+  git_config u["id"] do
+    options u["git"]
+    only_if { u["git"] }
   end
 
-  github_sync user["github"]["local_dir"] do
-    local_user user["id"]
-    users user["github"]["users"]
-    orgs user["github"]["orgs"]
+  github_sync u["github"]["local_dir"] do
+    local_user u["id"]
+    users u["github"]["users"]
+    orgs u["github"]["orgs"]
     only_if { node["users"]["github_sync"] }
   end
 
-  sublime user["id"] do
-    options user["sublime"]
-    only_if { user["sublime"] }
+  sublime u["id"] do
+    options u["sublime"]
+    only_if { u["sublime"] }
   end
 end
