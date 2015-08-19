f56eab8519d79271cd6efe35d9a9cbf0814ad6b3
Mike Conigliaro <mike@conigliaro.org>
2015-08-18 19:23:33 -0600

    Remove java and subsonic cookbooks

--- cookbooks/java/recipes/default.rb
+++
@@ -1,7 +0,0 @@
-#
-# Cookbook Name:: java
-# Recipe:: default
-#
-# Copyright 2012, Michael Paul Thomas Conigliaro
-#
-package "openjdk-6-jre"
--- cookbooks/subsonic/attributes/default.rb
+++
@@ -1,2 +0,0 @@
-default["subsonic"]["version"]      = "4.7"
-default["subsonic"]["download_url"] = "http://downloads.sourceforge.net/project/subsonic/subsonic/#{subsonic["version"]}/subsonic-#{subsonic["version"]}.deb"
