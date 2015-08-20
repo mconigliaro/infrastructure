25d1be4b15266b88bfb7d4e7b51d6cc5d103a436
Mike Conigliaro <mike@conigliaro.org>
2015-08-20 11:33:39 -0600

    Remove chef cookbook

--- cookbooks/chef/attributes/default.rb
+++
@@ -1,9 +0,0 @@
-default["chef"] = {
-  "client" => {
-    "daemon_enabled"         => false,
-    "interval"               => 1800,
-    "splay"                  => 20,
-    "chef_server_url"        => "http://localhost:4000",
-    "validation_client_name" => "chef-validator"
-  }
-}
