e551a5b1d3060af947ce973b1a97919784c8a6c0
Mike Conigliaro <mike@conigliaro.org>
2015-08-20 12:15:04 -0600

    Replace avahi with mconigliaro_avahi

---
+++ cookbooks/mconigliaro_avahi/README.md
@@ -0,0 +1,4 @@
+# mconigliaro_avahi
+
+TODO: Enter the cookbook description here.
+
---
+++ cookbooks/mconigliaro_avahi/metadata.rb
@@ -0,0 +1,9 @@
+name 'mconigliaro_avahi'
+maintainer 'Mike Conigliaro'
+maintainer_email 'mike@conigliaro.org'
+license 'all_rights'
+description 'Installs/Configures mconigliaro_avahi'
+long_description 'Installs/Configures mconigliaro_avahi'
+version '0.1.0'
+
+depends 'mconigliaro_monit'
