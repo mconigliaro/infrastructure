8e3c15ad5d8f2bba9f2ae1bb262a71e69675a9eb
Mike Conigliaro <mike@conigliaro.org>
2015-08-25 10:27:03 -0600

    Make recipes public

--- cookbooks/mconigliaro_ubuntu/recipes/default.rb
+++
@@ -1,7 +0,0 @@
-#
-# Cookbook Name:: mconigliaro_ubuntu
-# Recipe:: default
-#
-# Copyright (c) 2015 Mike Conigliaro, All Rights Reserved.
-include_recipe 'mconigliaro_ubuntu::_grub'
-include_recipe 'mconigliaro_ubuntu::_packages'
