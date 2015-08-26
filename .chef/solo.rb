f4963245ea300407d9a8f81274ba2eed85010371
Mike Conigliaro <mike@conigliaro.org>
2015-08-25 20:08:27 -0600

    Remove .chef directory

--- .chef/solo.rb
+++
@@ -1,9 +0,0 @@
-log_level       :info
-verbose_logging false
-file_cache_path "#{ENV['HOME']}/.chef/cache"
-file_backup_path "#{ENV['HOME']}/.chef/backup"
-cache_type      "BasicFile"
-cache_options({ :path => "#{ENV['HOME']}/.chef/cache/checksums", :skip_expires => true })
-cookbook_path   File.expand_path(File.join(File.dirname(__FILE__), "..", "cookbooks"))
-role_path       File.expand_path(File.join(File.dirname(__FILE__), "..", "roles"))
-data_bag_path   File.expand_path(File.join(File.dirname(__FILE__), "..", "data_bags"))
