092630598541e030eb0ac053ae8ef36553faf1cc
Mike Conigliaro <mike@conigliaro.org>
2015-08-28 12:34:54 -0600

    Don't error out if fqdn is not set

--- cookbooks/mconigliaro_postfix/recipes/default.rb
+++ cookbooks/mconigliaro_postfix/recipes/default.rb
@@ -34,7 +34,7 @@ end
 template '/etc/postfix/main.cf' do
   variables(
     inet_interfaces: node['mconigliaro_postfix']['inet_interfaces'].join(', '),
-    myhostname: node['fqdn'],
+    myhostname: node['fqdn'] || node['hostname'],
     virtual_alias_domains: node['mconigliaro_postfix']['virtual_alias_domains'].join(' '),
     relayhost: node['mconigliaro_postfix']['relayhost'],
     configure_smtpd_sasl: configure_smtpd_sasl,
