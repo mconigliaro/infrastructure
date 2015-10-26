2f352f74df5514debab69501cd107eeb09ce4ca1
Mike Conigliaro <mike@conigliaro.org>
2015-10-26 17:05:22 -0600

    Enable cups-browsed

--- cookbooks/mconigliaro_cups/recipes/default.rb
+++ cookbooks/mconigliaro_cups/recipes/default.rb
@@ -8,12 +8,16 @@ include_recipe 'mconigliaro_monit'
 package 'cups'
 
 # FIXME: Why is this here? It conflicts with the Upstart script.
-file '/etc/init.d/cups' do
-  action :delete
+%w(cups cups-browsed).each do |obj|
+  file "/etc/init.d/#{obj}" do
+    action :delete
+  end
 end
 
-service 'cups' do
-  action [:start, :enable]
+%w(cups cups-browsed).each do |obj|
+  service obj do
+    action [:start, :enable]
+  end
 end
 
 template '/etc/cups/cupsd.conf' do
