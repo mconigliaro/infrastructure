652d2a7ff35dbde4b1e0a557c688e656ca8da516
Mike Conigliaro <mike@conigliaro.org>
2017-04-26 15:42:47 -0600

    Update mconigliaro_avahi for Ubuntu 16.04 and Chef 13

--- cookbooks/mconigliaro_avahi/resources/service.rb
+++ cookbooks/mconigliaro_avahi/resources/service.rb
@@ -1,4 +1,23 @@
 actions :create, :delete
 default_action :create
 
-attribute :service_name, name_attribute: true
+property :service_name, name_attribute: true
+
+action :create do
+  service 'avahi-daemon'
+
+  template "/etc/avahi/services/#{new_resource.name}" do
+    source "#{new_resource.name}.avahi.erb"
+    mode '0644'
+    notifies :restart, 'service[avahi-daemon]'
+  end
+end
+
+action :delete do
+  service 'avahi-daemon'
+
+  file "/etc/avahi/services/#{new_resource.name}" do
+    action :delete
+    notifies :restart, 'service[avahi-daemon]'
+  end
+end
