ae1e4a51485f0dd4f0bb8664e5d9316588c4b8e5
Mike Conigliaro <mike@conigliaro.org>
2017-04-28 12:38:47 -0600

    Trivial

--- iso2usb.sh
+++ iso2usb.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-
+#
+# Usage: ./iso2usb <device> <iso>
+# Example: ./iso2usb.sh /dev/disk1 ~/Downloads/ubuntu-16.04.2-server-amd64.iso
+#
 set -ex
 
-DEV=$1 # get this with diskutil list
-ISO=${ISO-ubuntu-14.04.3-server-amd64.iso}
+DEV=$1 # Get the device name with `diskutil list`
+ISO=$2 # Path to ISO
 IMG=${ISO}.dmg
 
 # https://help.ubuntu.com/community/How%20to%20install%20Ubuntu%20on%20MacBook%20using%20USB%20Stick
