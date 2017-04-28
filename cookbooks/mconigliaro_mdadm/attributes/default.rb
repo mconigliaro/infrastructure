fce66374ecf3c9e5b356a9281019df2cd2877658
Mike Conigliaro <mike@conigliaro.org>
2017-04-28 13:21:57 -0600

    Change md device name

--- cookbooks/mconigliaro_mdadm/attributes/default.rb
+++ cookbooks/mconigliaro_mdadm/attributes/default.rb
@@ -1,4 +1,4 @@
 default['mconigliaro_mdadm']['raid_level'] = 1
 default['mconigliaro_mdadm']['volumes'] = '/dev/sd{b,c}'
-default['mconigliaro_mdadm']['raid_device'] = '/dev/md0'
+default['mconigliaro_mdadm']['raid_device'] = '/dev/md127' # Default device from Ubuntu installer
 default['mconigliaro_mdadm']['mount_point'] = '/mnt/data'
