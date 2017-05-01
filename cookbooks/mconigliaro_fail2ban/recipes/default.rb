d0343e7af7a87bf6a6b802f46efaf330fd3a6166
Mike Conigliaro <mike@conigliaro.org>
2017-05-01 15:06:47 -0600

    Correctly enable fail2ban for dovecot

--- cookbooks/mconigliaro_fail2ban/recipes/default.rb
+++ cookbooks/mconigliaro_fail2ban/recipes/default.rb
@@ -7,7 +7,7 @@ package 'fail2ban'
 
 template '/etc/fail2ban/jail.local' do
   variables(
-    dovecot_enabled: node['recipes'].include?('mconigliaro_dovecot::default'),
+    dovecot_enabled: node['recipes'].include?('mconigliaro_dovecot::imap'),
     postfix_enabled: node['recipes'].include?('mconigliaro_postfix::amavis')
   )
   mode '0644'
