8f9a79e54e6fed1ff3824a9270731bb8387d31e0
Mike Conigliaro <mike@conigliaro.org>
2018-08-07 12:55:07 -0600

    Add some DNS records

--- terraform/providers/aws/us-east-1/route53.tf
+++ terraform/providers/aws/us-east-1/route53.tf
@@ -3,7 +3,31 @@ resource "aws_route53_zone" "conigliaro_org" {
   comment = ""
 }
 
+resource "aws_route53_record" "mx_conigliaro_org" {
+  zone_id = "${aws_route53_zone.conigliaro_org.id}"
+  name = "${aws_route53_zone.conigliaro_org.name}"
+  type = "MX"
+  ttl = "300"
+  records = [ "10 mail.${aws_route53_zone.conigliaro_org.name}" ]
+}
+
 resource "aws_route53_zone" "gyrate_org" {
   name = "gyrate.org."
   comment = ""
+}
+
+resource "aws_route53_record" "mx_gyrate_org" {
+  zone_id = "${aws_route53_zone.gyrate_org.id}"
+  name = "${aws_route53_zone.gyrate_org.name}"
+  type = "MX"
+  ttl = "300"
+  records = [ "10 mail.${aws_route53_zone.gyrate_org.name}" ]
+}
+
+resource "aws_route53_record" "cname_bloarzeyd_gyrate_org" {
+  zone_id = "${aws_route53_zone.gyrate_org.id}"
+  name = "bloarzeyd.${aws_route53_zone.gyrate_org.name}"
+  type = "CNAME"
+  ttl = "300"
+  records = [ "bloarzeyd.github.com" ]
 }
