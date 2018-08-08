9d1cf2b79a6a9b8999d5366ffd58e560b7818386
Mike Conigliaro <mike@conigliaro.org>
2018-08-08 12:43:08 -0600

    Add DNS aliases

--- terraform/providers/aws/us-east-1/route53.tf
+++ terraform/providers/aws/us-east-1/route53.tf
@@ -3,6 +3,28 @@ resource "aws_route53_zone" "conigliaro_org" {
   comment = ""
 }
 
+resource "aws_route53_record" "a_conigliaro_org" {
+  zone_id = "${aws_route53_zone.conigliaro_org.id}"
+  name = "${aws_route53_zone.conigliaro_org.name}"
+  type = "A"
+  alias {
+    name = "www.${aws_route53_zone.conigliaro_org.name}"
+    zone_id = "${aws_route53_zone.conigliaro_org.id}"
+    evaluate_target_health = false
+  }
+}
+
+resource "aws_route53_record" "a_www_conigliaro_org" {
+  zone_id = "${aws_route53_zone.conigliaro_org.id}"
+  name = "www.${aws_route53_zone.conigliaro_org.name}"
+  type = "A"
+  alias {
+    name = "${aws_s3_bucket.www_conigliaro_org.website_domain}"
+    zone_id = "${aws_s3_bucket.www_conigliaro_org.hosted_zone_id}"
+    evaluate_target_health = false
+  }
+}
+
 resource "aws_route53_record" "mx_conigliaro_org" {
   zone_id = "${aws_route53_zone.conigliaro_org.id}"
   name = "${aws_route53_zone.conigliaro_org.name}"
@@ -14,6 +36,28 @@ resource "aws_route53_record" "mx_conigliaro_org" {
 resource "aws_route53_zone" "gyrate_org" {
   name = "gyrate.org."
   comment = ""
+}
+
+resource "aws_route53_record" "a_gyrate_org" {
+  zone_id = "${aws_route53_zone.gyrate_org.id}"
+  name = "${aws_route53_zone.gyrate_org.name}"
+  type = "A"
+  alias {
+    name = "www.${aws_route53_zone.gyrate_org.name}"
+    zone_id = "${aws_route53_zone.gyrate_org.id}"
+    evaluate_target_health = false
+  }
+}
+
+resource "aws_route53_record" "a_www_gyrate_org" {
+  zone_id = "${aws_route53_zone.gyrate_org.id}"
+  name = "www.${aws_route53_zone.gyrate_org.name}"
+  type = "A"
+  alias {
+    name = "${aws_s3_bucket.www_gyrate_org.website_domain}"
+    zone_id = "${aws_s3_bucket.www_gyrate_org.hosted_zone_id}"
+    evaluate_target_health = false
+  }
 }
 
 resource "aws_route53_record" "mx_gyrate_org" {
