ca2ff391b9f1433f26f68247b48e31895cce530c
Mike Conigliaro <mike@conigliaro.org>
2019-01-27 11:36:02 -0700

    Add fail2ban role

--- ansible/roles/fail2ban/molecule/default/tests/test_default.py
+++ ansible/roles/fail2ban/molecule/default/tests/test_default.py
@@ -6,5 +6,5 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_sudo(host):
-    host.exists('sudo')
+def test_fail2ban(host):
+    assert host.exists('fail2ban-server')
