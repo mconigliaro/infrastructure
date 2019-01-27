2d7d9cc5168d6fd2af294105545ecd1ef4175de8
Mike Conigliaro <mike@conigliaro.org>
2019-01-26 17:10:21 -0700

    Add postfix tests

--- ansible/roles/postfix/molecule/default/tests/test_default.py
+++ ansible/roles/postfix/molecule/default/tests/test_default.py
@@ -6,9 +6,18 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+def test_postfix_listening_on_all_interfaces(host):
+    assert 'tcp://0.0.0.0:25' in host.socket.get_listening_sockets()
+    assert 'tcp://0.0.0.0:587' in host.socket.get_listening_sockets()
 
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+
+def test_postfix_listening_on_loopback_interface(host):
+    assert 'tcp://127.0.0.1:10025' in host.socket.get_listening_sockets()
+
+
+def test_postgrey_listening_on_loopback_interface(host):
+    assert 'tcp://127.0.0.1:10023' in host.socket.get_listening_sockets()
+
+
+def test_amavis_listening_on_loopback_interface(host):
+    assert 'tcp://127.0.0.1:10024' in host.socket.get_listening_sockets()
