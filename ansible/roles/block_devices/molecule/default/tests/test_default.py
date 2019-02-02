93bf028ad016248e65817f2cca1932d68a4d0cd0
Mike Conigliaro <mike@conigliaro.org>
2019-02-02 15:12:56 -0700

    Refactor block_devices and data_volumes

--- ansible/roles/block_devices/molecule/default/tests/test_default.py
+++ ansible/roles/block_devices/molecule/default/tests/test_default.py
@@ -6,6 +6,13 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_block_devices(host):
+def test_block_files(host):
+    assert host.file('/tmp/test0').exists
     assert host.file('/tmp/test1').exists
     assert host.file('/tmp/test2').exists
+
+
+def test_block_devices(host):
+    assert host.run_expect([0], 'losetup /dev/loop0')
+    assert host.run_expect([0], 'losetup /dev/loop1')
+    assert host.run_expect([0], 'losetup /dev/loop2')
--- ansible/roles/data_volumes/molecule/default/tests/test_default.py
+++ ansible/roles/data_volumes/molecule/default/tests/test_default.py
@@ -7,10 +7,14 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 
 
 def test_data_volumes(host):
+    assert host.mount_point('/mnt/test0').exists
+    assert host.mount_point('/mnt/test0').device == '/dev/loop0'
+    assert host.mount_point('/mnt/test0').filesystem == 'ext4'
+
     assert host.mount_point('/mnt/test1').exists
-    assert host.mount_point('/mnt/test1').device == '/dev/loop0'
+    assert host.mount_point('/mnt/test1').device == '/dev/loop1'
     assert host.mount_point('/mnt/test1').filesystem == 'ext4'
 
     assert host.mount_point('/mnt/test2').exists
-    assert host.mount_point('/mnt/test2').device == '/dev/loop1'
+    assert host.mount_point('/mnt/test2').device == '/dev/loop2'
     assert host.mount_point('/mnt/test2').filesystem == 'ext4'
