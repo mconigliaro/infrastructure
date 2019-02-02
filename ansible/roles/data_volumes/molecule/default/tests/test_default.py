a15548682405fe38ef434142f0894698c522661a
Mike Conigliaro <mike@conigliaro.org>
2019-02-02 09:47:43 -0700

    Add data_volumes module

---
+++ ansible/roles/data_volumes/molecule/default/tests/test_default.py
@@ -0,0 +1,16 @@
+import os
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+def test_data_volumes(host):
+    assert host.mount_point('/mnt/test').exists
+    assert host.mount_point('/mnt/test').device == '/dev/loop0'
+    assert host.mount_point('/mnt/test').filesystem == 'ext4'
+
+    assert host.mount_point('/mnt/test2').exists
+    assert host.mount_point('/mnt/test2').device == '/dev/loop1'
+    assert host.mount_point('/mnt/test2').filesystem == 'ext4'
