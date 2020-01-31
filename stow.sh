54514ec2647f8535a044b0ba32e83a4c65048c8a
Mike Conigliaro <mike@conigliaro.org>
2020-01-30 18:13:46 -0700

    Refactor stow files

--- stow.sh
+++ stow.sh
@@ -4,57 +4,62 @@ set -eu
 
 cd "$(dirname "${BASH_SOURCE[0]}")/stow"
 
-stow_options='--verbose' # Add --delete to unstow
+stow_options="--verbose $*" # Use --delete to unstow
 brew_prefix=$(brew --prefix)
 
 # Miscellaneous scripts
 stow $stow_options --target "$brew_prefix/bin" bin
 
 # Git
-stow $stow_options --target ~ git
+stow $stow_options --target "$HOME" git
 
 # Gnupg
-stow $stow_options --target ~ gnupg
+stow $stow_options --target "$HOME/.gnupg" gnupg
 
 # OpenSSH
-if [[ ! -d ~/.ssh ]]; then
-  mkdir ~/.ssh
-  ssh-keygen -t rsa -b 4096 -o -a 100 -f ~/.ssh/id_rsa
+if [[ ! -d "$HOME/.ssh" ]]; then
+    mkdir "$HOME/.ssh"
+    ssh-keygen -t rsa -b 4096 -o -a 100 -f "$HOME/.ssh/id_rsa"
 fi
-stow $stow_options --target ~ openssh
+stow $stow_options --target "$HOME/.ssh" openssh
 
 # Python
-stow $stow_options --target ~ python
+stow $stow_options --target "$HOME" python
 
 # Ruby
-stow $stow_options --target ~ ruby
+stow $stow_options --target "$HOME" ruby
 
 # Squid
-if [[ -e "$brew_prefix/etc/squid.conf" && ! -L "$brew_prefix/etc/squid.conf" ]]; then
-  rm "$brew_prefix/etc/squid.conf"
+squid_conf="$brew_prefix/etc/squid.conf"
+if [[ -e "$squid_conf" && ! -L "$squid_conf" ]]; then
+    mv "$squid_conf" "$squid_conf-orig"
 fi
 stow $stow_options --target "$brew_prefix/etc" squid
 if [[ ! -d "$brew_prefix/var/cache/squid/00" ]]; then
-  squid -z
+    squid -z
 fi
 brew services restart squid
 
 # Sublime Text
-killall -v 'Sublime Text' || true
-stow $stow_options --target ~ sublime-text
+sublime_package_dir="$HOME/Library/Application Support/Sublime Text 3/Packages"
+if [[ -e "$sublime_package_dir/User" && ! -L "$sublime_package_dir/User" ]]; then
+    killall -v 'Sublime Text' || true
+    mv "$sublime_package_dir/User" "$sublime_package_dir/User-orig"
+fi
+stow $stow_options --target "$sublime_package_dir" sublime-text
 
 # Test Kitchen
-mkdir -p ~/.kitchen
-stow $stow_options --target ~ test-kitchen
+mkdir -p "$HOME/.kitchen"
+stow $stow_options --target "$HOME/.kitchen" test-kitchen
 
 # Tor
 stow $stow_options --target "$brew_prefix/etc/tor" tor
 
 # ZSH
-ZSH_PATH="$brew_prefix/bin/zsh"
-if [[ "$SHELL" != "$ZSH_PATH" ]]; then
-  sudo chsh -s "$ZSH_PATH" "$USER"
+zsh_path="$brew_prefix/bin/zsh"
+if [[ "$SHELL" != "$zsh_path" ]]; then
+    sudo chsh -s "$zsh_path" "$USER"
 fi
-stow $stow_options --target ~ zsh
+stow $stow_options --target "$HOME" zsh
 
 hash -r
