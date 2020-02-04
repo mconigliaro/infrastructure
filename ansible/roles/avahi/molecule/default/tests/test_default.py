bd1c7fa2feb6de25d18cc9e645b62b3e7d2806b3
Mike Conigliaro <mike@conigliaro.org>
2020-02-03 20:50:53 -0700

    Add dbus dependency and fix tests

--- ansible/roles/avahi/molecule/default/tests/test_default.py
+++ ansible/roles/avahi/molecule/default/tests/test_default.py
@@ -1,11 +1,10 @@
-# FIXME: Why isn't it running?
-# def test_avahi_is_running(host):
-#     assert host.service('avahi-daemon').is_running
+def test_avahi_is_running(host):
+    assert host.service('avahi-daemon').is_running
 
 
-# def test_avahi_is_enabled(host):
-#     assert host.service('avahi-daemon').is_enabled
+def test_avahi_is_enabled(host):
+    assert host.service('avahi-daemon').is_enabled
 
 
-# def test_avahi_is_listening(host):
-#     assert 'tcp://0.0.0.0:5353' in host.socket.get_listening_sockets()
+def test_avahi_is_listening(host):
+    assert 'udp://0.0.0.0:5353' in host.socket.get_listening_sockets()
