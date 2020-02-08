ae92e9db9b54a1a9912336fcdfda30f3c5dfb3db
Mike Conigliaro <mike@conigliaro.org>
2020-02-08 14:28:49 -0700

    Trivial

--- ansible/roles/rspamd/molecule/default/tests/test_default.py
+++ ansible/roles/rspamd/molecule/default/tests/test_default.py
@@ -6,13 +6,13 @@ def test_rspamd_is_enabled(host):
     assert host.service('rspamd').is_enabled
 
 
-def test_rspamd_proxy_is_listening(host):
+def test_rspamd_proxy_worker_is_listening(host):
     assert 'tcp://127.0.0.1:11332' in host.socket.get_listening_sockets()
 
 
-def test_rspamd_milter_is_listening(host):
+def test_rspamd_normal_worker_is_listening(host):
     assert 'tcp://127.0.0.1:11333' in host.socket.get_listening_sockets()
 
 
-def test_rspamd_web_is_listening(host):
+def test_rspamd_controller_worker_is_listening(host):
     assert 'tcp://127.0.0.1:11334' in host.socket.get_listening_sockets()
