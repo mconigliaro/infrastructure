c68384f39e6f001516b6b8d6324a07d0c032648e
Mike Conigliaro <mike@conigliaro.org>
2020-03-05 12:23:13 -0700

    Improve dovecot configuration

--- ansible/roles/dovecot/molecule/default/tests/test_default.py
+++ ansible/roles/dovecot/molecule/default/tests/test_default.py
@@ -1,10 +1,14 @@
 def test_dovecot_is_running(host):
-    assert host.service('dovecot').is_running
+    assert host.service("dovecot").is_running
 
 
 def test_dovecot_is_enabled(host):
-    assert host.service('dovecot').is_enabled
+    assert host.service("dovecot").is_enabled
 
 
 def test_dovecot_is_listening(host):
-    assert 'tcp://0.0.0.0:143' in host.socket.get_listening_sockets()
+    assert "tcp://0.0.0.0:143" in host.socket.get_listening_sockets()
+
+
+def test_dovecot_sieve_before_scripts(host):
+    assert host.file("/var/lib/dovecot/sieve.d/10-junk.svbin").exists
