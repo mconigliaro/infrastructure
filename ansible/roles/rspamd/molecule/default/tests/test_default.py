84d93353d3fd0791ef78c7cc997f01c5a09b1f68
Mike Conigliaro <mike@conigliaro.org>
2020-03-04 21:02:43 -0700

    Add rspamd config test

--- ansible/roles/rspamd/molecule/default/tests/test_default.py
+++ ansible/roles/rspamd/molecule/default/tests/test_default.py
@@ -1,18 +1,22 @@
+def test_rspamadm_configtest(host):
+    assert host.run_expect([0], "rspamadm configtest")
+
+
 def test_rspamd_is_running(host):
-    assert host.service('rspamd').is_running
+    assert host.service("rspamd").is_running
 
 
 def test_rspamd_is_enabled(host):
-    assert host.service('rspamd').is_enabled
+    assert host.service("rspamd").is_enabled
 
 
 def test_rspamd_proxy_worker_is_listening(host):
-    assert 'tcp://127.0.0.1:11332' in host.socket.get_listening_sockets()
+    assert "tcp://127.0.0.1:11332" in host.socket.get_listening_sockets()
 
 
 def test_rspamd_normal_worker_is_listening(host):
-    assert 'tcp://127.0.0.1:11333' in host.socket.get_listening_sockets()
+    assert "tcp://127.0.0.1:11333" in host.socket.get_listening_sockets()
 
 
 def test_rspamd_controller_worker_is_listening(host):
-    assert 'tcp://127.0.0.1:11334' in host.socket.get_listening_sockets()
+    assert "tcp://127.0.0.1:11334" in host.socket.get_listening_sockets()
