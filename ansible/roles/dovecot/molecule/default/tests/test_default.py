badfd6e6d0168cd3d5db88221fd1b824d096be6d
Mike Conigliaro <mike@conigliaro.org>
2020-03-05 20:04:30 -0700

    Simplify dovecot role

--- ansible/roles/dovecot/molecule/default/tests/test_default.py
+++ ansible/roles/dovecot/molecule/default/tests/test_default.py
@@ -10,5 +10,21 @@ def test_dovecot_is_listening(host):
     assert "tcp://0.0.0.0:143" in host.socket.get_listening_sockets()
 
 
+def test_dovecot_auth_socket(host):
+    file = host.file("/var/spool/postfix/private/dovecot-auth")
+    assert file.exists
+    assert file.user == "postfix"
+    assert file.group == "postfix"
+    assert file.mode == 0o666
+
+
+def test_dovecot_lmtp_socket(host):
+    file = host.file("/var/spool/postfix/private/dovecot-lmtp")
+    assert file.exists
+    assert file.user == "postfix"
+    assert file.group == "postfix"
+    assert file.mode == 0o666
+
+
 def test_dovecot_sieve_before_scripts(host):
-    assert host.file("/var/lib/dovecot/sieve.d/10-junk.svbin").exists
+    assert host.file("/var/lib/dovecot/sieve_before.d/10-junk.svbin").exists
