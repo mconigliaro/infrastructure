ed089c1993a9db999ce518803f123231959bb485
Mike Conigliaro <mike@conigliaro.org>
2020-03-08 21:54:28 -0600

    Delete dbus role

--- ansible/roles/avahi/molecule/default/tests/test_default.py
+++ ansible/roles/avahi/molecule/default/tests/test_default.py
@@ -1,10 +1,25 @@
+def test_dbus_is_running(host):
+    assert host.service("dbus").is_running
+
+
+def test_dbus_is_enabled(host):
+    assert host.service("dbus").is_enabled
+
+
+# FIXME: Fails on bionic
+def test_dbus_is_listening(host):
+    assert (
+        "unix:///var/run/dbus/system_bus_socket" in host.socket.get_listening_sockets()
+    )
+
+
 def test_avahi_is_running(host):
-    assert host.service('avahi-daemon').is_running
+    assert host.service("avahi-daemon").is_running
 
 
 def test_avahi_is_enabled(host):
-    assert host.service('avahi-daemon').is_enabled
+    assert host.service("avahi-daemon").is_enabled
 
 
 def test_avahi_is_listening(host):
-    assert 'udp://0.0.0.0:5353' in host.socket.get_listening_sockets()
+    assert "udp://0.0.0.0:5353" in host.socket.get_listening_sockets()
