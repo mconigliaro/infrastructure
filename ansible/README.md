2339c17a0cc54e6c94d4829fec0850b329ea864c
Mike Conigliaro <mike@conigliaro.org>
2020-03-09 20:44:32 -0600

    Updates for bionic

--- ansible/README.md
+++ ansible/README.md
@@ -33,8 +33,6 @@ All commands are expected to be run inside a pipenv shell:
 - Fix postfix mail forwarding
     - https://serverfault.com/questions/681278/forwarding-email-with-postfix-via-aws-ses
     - https://serverfault.com/questions/499955/aws-ses-email-address-is-not-verified-error-with-postfix-relay/634366#634366
-- Upgrade to bionic/18.04
 - Test focal/20.04
 - Upgrade to focal/20.04
-- Use modern config from https://ssl-config.mozilla.org everywhere
 - Ansible 3.0: Delete obsolete `ansible/roles/*/molecule/default/Dockerfile.j2`
--- ansible/roles/packages/molecule/default/tests/test_default.py
+++ ansible/roles/packages/molecule/default/tests/test_default.py
@@ -1,82 +1,50 @@
 def test_bsd_mailx(host):
-    assert host.exists('mail')
-
-
-def test_chkrootkit(host):
-    assert host.exists('chkrootkit')
+    assert host.exists("mail")
 
 
 def test_curl(host):
-    assert host.exists('curl')
+    assert host.exists("curl")
 
 
 def test_htop(host):
-    assert host.exists('htop')
-
-
-def test_iftop(host):
-    assert host.exists('iftop')
-
-
-def test_iotop(host):
-    assert host.exists('iotop')
-
-
-def test_iptraf_ng(host):
-    assert host.exists('iptraf-ng')
+    assert host.exists("htop")
 
 
 def test_lsof(host):
-    assert host.exists('lsof')
+    assert host.exists("lsof")
 
 
 def test_mtr(host):
-    assert host.exists('mtr')
-
-
-def test_ngrep(host):
-    assert host.exists('ngrep')
-
-
-def test_nethogs(host):
-    assert host.exists('nethogs')
-
-
-def test_nmap(host):
-    assert host.exists('nmap')
+    assert host.exists("mtr")
 
 
 def test_psmisc(host):
-    assert host.exists('pkill')
+    assert host.exists("pkill")
 
 
 def test_silversearcher_ag(host):
-    assert host.exists('ag')
+    assert host.exists("ag")
 
 
 def test_strace(host):
-    assert host.exists('strace')
+    assert host.exists("strace")
 
 
 def test_tcpdump(host):
-    assert host.exists('tcpdump')
+    assert host.exists("tcpdump")
 
 
 def test_telnet(host):
-    assert host.exists('telnet')
+    assert host.exists("telnet")
 
 
 def test_tmux(host):
-    assert host.exists('tmux')
-
-
-def test_tshark(host):
-    assert host.exists('tshark')
+    assert host.exists("tmux")
 
 
 def test_usbmount(host):
-    assert host.exists('/usr/share/usbmount/usbmount')
+    assert host.exists("/usr/share/usbmount/usbmount")
 
 
 def test_vim(host):
-    assert host.exists('vim')
+    assert host.exists("vim")
