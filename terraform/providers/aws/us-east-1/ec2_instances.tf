e329ae7cddd5125db6029567655731c8859b635f
Mike Conigliaro <mike@conigliaro.org>
2020-03-09 18:35:03 -0600

    Replace mail instance

--- terraform/providers/aws/us-east-1/ec2_instances.tf
+++ terraform/providers/aws/us-east-1/ec2_instances.tf
@@ -1,21 +1,3 @@
-resource "aws_instance" "mail-2017-09-12" {
-  ami           = "ami-80861296" # us-east-1 xenial hvm ssd
-  instance_type = "t2.micro"
-  key_name      = aws_key_pair.mike.key_name
-  subnet_id     = module.vpc.public_subnets[0]
-
-  vpc_security_group_ids = [
-    module.vpc.default_security_group_id,
-    aws_security_group.mail.id,
-  ]
-
-  tags = {
-    Name = "mail-2017-09-12"
-  }
-
-  disable_api_termination = true
-}
-
 resource "aws_ebs_volume" "data" {
   availability_zone = "us-east-1b"
   type              = "standard"
@@ -28,6 +10,28 @@ resource "aws_ebs_volume" "data" {
 
 resource "aws_volume_attachment" "mail_data" {
   device_name = "/dev/sdf"
-  instance_id = aws_instance.mail-2017-09-12.id
+  instance_id = aws_instance.mail-2020-03-09.id
   volume_id   = aws_ebs_volume.data.id
+}
+
+resource "aws_instance" "mail-2020-03-09" {
+  ami           = "ami-046842448f9e74e7d" # us-east-1 bionic hvm ssd
+  instance_type = "t2.micro"
+  key_name      = aws_key_pair.mike.key_name
+  subnet_id     = module.vpc.public_subnets[0]
+
+  vpc_security_group_ids = [
+    module.vpc.default_security_group_id,
+    aws_security_group.mail.id,
+  ]
+
+  tags = {
+    Name = "mail-2020-03-09"
+  }
+
+  volume_tags = {
+    Name = "mail-2020-03-09"
+  }
+
+  disable_api_termination = true
 }
--- terraform/providers/aws/us-east-1/elastic_ips.tf
+++ terraform/providers/aws/us-east-1/elastic_ips.tf
@@ -1,5 +1,5 @@
 resource "aws_eip" "mail" {
-  instance = aws_instance.mail-2017-09-12.id
+  instance = aws_instance.mail-2020-03-09.id
   vpc      = true
 
   tags = {
