963a18b2d4c9c3627bf4c496d97e7c95a9ecad59
Mike Conigliaro <mike@conigliaro.org>
2020-03-10 10:38:56 -0600

    Remove sysstat role

--- ansible/roles/sysstat/molecule/default/tests/test_default.py
+++
@@ -1,7 +0,0 @@
-# FIXME: Fails in Docker
-def test_sysstat_is_running(host):
-    assert host.service("sysstat").is_running
-
-
-def test_sysstat_is_enabled(host):
-    assert host.service("sysstat").is_enabled
