30bea23c2b815f23c8af62992260216164dd3eb6
Mike Conigliaro <mike@conigliaro.org>
2020-04-06 18:35:13 -0600

    Re-enable test

--- ansible/roles/monit/molecule/default/tests/test_default.py
+++ ansible/roles/monit/molecule/default/tests/test_default.py
@@ -5,6 +5,6 @@ def test_monit_is_running(host):
 def test_monit_is_enabled(host):
     assert host.service('monit').is_enabled
 
-# FIXME: Needs net-tools installed
-# def test_monit_is_listening(host):
-#     assert 'tcp://127.0.0.1:2812' in host.socket.get_listening_sockets()
+
+def test_monit_is_listening(host):
+    assert 'tcp://127.0.0.1:2812' in host.socket.get_listening_sockets()
